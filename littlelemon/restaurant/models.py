a4d2523bf2e8f748b77a9399ad23d9cab03d4581
unknown <tanmaypatel.2k1@gmail.com>
2024-02-18 14:47:49 -0500

    __

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Bookings(models.Model):
+    Booking_ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 255)
+    No_of_guests = models.IntegerField(6)
+    Booking_Date = models.DateField()
+
+class Menu(models.Model):
+    Menu_ID = models.AutoField(primary_key=True, default=0)
+    Title = models.CharField(max_length = 255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(5)
