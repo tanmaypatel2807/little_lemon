0f7cd59b82f81b17529939ec5e77697ab5a489a5
unknown <tanmaypatel.2k1@gmail.com>
2024-02-18 20:21:41 -0500

    Final Commit

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # Other paths...
+    path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view(), name='menu-list-create'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
     # path('bookings/', views.BookingViewSet.as_view()),
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -11,6 +11,9 @@ from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here. 
+def index(request):
+  return render(request, 'index.html', {})
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
